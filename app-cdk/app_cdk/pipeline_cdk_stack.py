edade9727939077ac114b3d3f4d97bd78ddb97f6
Victor <victor.carneiro@dataprev.gov.br>
2025-05-07 17:04:09 +0000

    Implemented CI pipeline

--- app-cdk/app_cdk/pipeline_cdk_stack.py
+++ app-cdk/app_cdk/pipeline_cdk_stack.py
@@ -41,7 +41,7 @@ class PipelineCdkStack(Stack):
 
         source_action = codepipeline_actions.CodeStarConnectionsSourceAction(
           action_name = 'GitHub',
-          owner = "{{organizationName}}",
+          owner = "Victor",
           repo = "cicd-workshop",
           output = source_output,
           branch = "main",
